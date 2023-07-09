c09accf95e2c6330d815f9742e10e52d14ce5254
TMoCo <thomas.moreno.cooper@gmail.com>
2023-07-09 17:24:06 +0100

    remove get endpoint

--- dev/index.js
+++ dev/index.js
@@ -6,10 +6,6 @@ const port = process.env.PORT
 
 app.use(express.static(path.join(__dirname, '../dist')))
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
