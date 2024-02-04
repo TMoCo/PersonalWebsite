fe04c90c2cbd975f23468e3e70e1e566f669dcfa
Thomas Moreno Cooper <thomas.cooper@bbc.co.uk>
2024-02-04 23:23:33 +0000

    chore: add app router + home page

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,23 @@
-import { loadEnv } from 'vite'
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    root: './src',
-    build: {
-      outDir: '../dist',
-      rollupOptions: {
-        external: ['react', 'react-dom'],
-        output: {
-          globals: {
-            react: 'React',
-            'react-dom': 'ReactDOM',
-          },
+export default defineConfig(() => ({
+  root: './src',
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
-    test: {
-      include: ['../test/**/*.test.ts'],
-    },
-    plugins: [react()],
-  }
-})
+  },
+  test: {
+    include: ['../test/**/*.test.ts'],
+  },
+  plugins: [react()],
+}))
