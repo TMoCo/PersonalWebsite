3edf8c42137350ccd4a1761a9bc9c5135e728f81
Thomas Moreno Cooper <thomas.cooper@bbc.co.uk>
2024-02-09 20:58:35 +0000

    chore: add mdx

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import mdx from '@mdx-js/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
@@ -19,5 +20,5 @@ export default defineConfig(() => ({
   test: {
     include: ['../test/**/*.test.ts']
   },
-  plugins: [react()]
+  plugins: [react(), mdx()]
 }))
