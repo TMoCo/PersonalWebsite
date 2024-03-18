1adbf0374038878b5321b9449fb264257ffdb802
tmoco <thomas.moreno.cooper@gmail.com>
2024-03-18 16:19:29 +0000

    chore: add route basename

--- src/App.tsx
+++ src/App.tsx
@@ -7,37 +7,43 @@ import Home from './pages/Home.mdx'
 import ErrorPage from './pages/ErrorPage'
 import About from './pages/About.mdx'
 
-import Portfolio from './pages/Portfolio.tsx'
+import Portfolio from './pages/Portfolio'
 import PostPage from './pages/Post'
 
 import portfolioLoader from './data/loaders/portfolioLoader'
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      errorElement: <ErrorPage />,
+      element: <Root routes={['about', 'portfolio']} />,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/about',
+          element: <About />
+        },
+        {
+          path: '/portfolio',
+          loader: portfolioLoader,
+          element: <Portfolio />
+        },
+        {
+          path: '/portfolio/:postId',
+          element: <PostPage />
+        }
+      ]
+    }
+  ],
   {
-    path: '/',
-    errorElement: <ErrorPage />,
-    element: <Root routes={['about', 'portfolio']} />,
-    children: [
-      {
-        path: '/',
-        element: <Home />
-      },
-      {
-        path: '/about',
-        element: <About />
-      },
-      {
-        path: '/portfolio',
-        loader: portfolioLoader,
-        element: <Portfolio />
-      },
-      {
-        path: '/portfolio/:postId',
-        element: <PostPage />
-      }
-    ]
+    // TODO: remove when replacing old Jekyll app
+    basename: 'PersonalWebsite'
   }
-])
+)
 
 const App = () => {
   return <RouterProvider router={router} />
