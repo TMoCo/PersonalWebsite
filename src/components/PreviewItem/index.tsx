7db6fbef922cb27404a6fe1acc8f90f6518b0d8d
Thomas Moreno Cooper <thomas.cooper@bbc.co.uk>
2024-09-08 18:44:43 +0200

    chore: better preview item style, use css instead of js

--- src/components/PreviewItem/index.tsx
+++ src/components/PreviewItem/index.tsx
@@ -15,18 +15,19 @@ const PreviewItem = ({
   justifyImgRight = false,
   children: description
 }: PropsWithChildren<PreviewItemProps>) => {
-  const PreviewInfo = [
-    <img key="preview-img" src={imgUri} className="preview-img"></img>,
-    <div key="preview-description" className="preview-info">
-      <h2 className="preview-title">
-        <Link className="preview-link" to={route}>
-          {title}
-        </Link>
-      </h2>
-      <p>{description}</p>
+  return (
+    <div className={`preview-item ${justifyImgRight ? 'rtl' : ''}`}>
+      <img src={imgUri} className="thumbnail"></img>
+      <div className="info">
+        <h2 className="title">
+          <Link className="link" to={route}>
+            {title}
+          </Link>
+        </h2>
+        <p>{description}</p>
+      </div>
     </div>
-  ]
-  return <div className="preview-item">{justifyImgRight ? PreviewInfo : PreviewInfo.reverse()}</div>
+  )
 }
 
 export default memo(PreviewItem)
