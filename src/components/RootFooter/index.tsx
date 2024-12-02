b3e90a6cef0c8cf7d26d95bf0977405fb7822714
Thomas Moreno Cooper <thomas.cooper@bbc.co.uk>
2024-12-02 10:22:06 +0000

    chore: remove comments

--- src/components/RootFooter/index.tsx
+++ src/components/RootFooter/index.tsx
@@ -1,21 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-// interface RootFooterProps {
-//   routes?: string[]
-// }
-
-// const RootFooter = ({ routes }: RootFooterProps) => {
 const RootFooter = () => {
   return (
     <footer className="root-footer">
       <div className="section">
         <Link to={'/'}>Thomas Moreno Cooper</Link>
       </div>
-      <div className="section">
-        Site contents © 2021 under MIT license.
-        {/* <Link to={'/'}>Thomas Moreno Cooper</Link> */}
-      </div>
+      <div className="section">Site contents © 2021 under MIT license.</div>
     </footer>
   )
 }
