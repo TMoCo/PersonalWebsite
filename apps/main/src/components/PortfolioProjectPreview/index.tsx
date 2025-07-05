28f6159d31bca9488d5d92870dfe88cc4894bb7f
Thomas Moreno Cooper <thomas.cooper@bbc.co.uk>
2025-07-05 23:27:28 +0200

    chore: github link to projects

--- apps/main/src/components/PortfolioProjectPreview/index.tsx
+++ apps/main/src/components/PortfolioProjectPreview/index.tsx
@@ -9,14 +9,14 @@ interface PreviewItemProps {
 }
 
 const PortfolioProjectPreview = ({ index, justifyImgRight = false }: PreviewItemProps) => {
-  const { setProject, meta } = usePortfolioContext()
+  const { setProjectIndex, meta } = usePortfolioContext()
   const { thumbnail, slug, title, excerpt } = meta[index]
   return (
     <div className={`preview-item ${justifyImgRight ? 'rtl' : ''}`}>
       <img src={thumbnail} className="thumbnail"></img>
       <div className="info">
         <h2 className="title">
-          <Link className="link" to={`/portfolio/${slug}`} onClick={() => setProject(index)}>
+          <Link className="link" to={`/portfolio/${slug}`} onClick={() => setProjectIndex(index)}>
             {title}
           </Link>
         </h2>
